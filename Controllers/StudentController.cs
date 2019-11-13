e99000e652d0f0f860f87d8d727818b0057cd8e7
Felipe Barbosa de Oliveira <felipe.boliveira@outlook.com>
2019-11-13 13:29:11 -0300

    Web App API: Commit inicial - POST funcionando - Base em JSON

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webApp.Models;
+
+namespace webApp.Controllers
+{
+    public class StudentController : ApiController
+    {
+        // GET: api/Student
+        public IEnumerable<Students> Get()
+        {
+            Students student = new Students();
+
+            return student.studentsList();
+        }
+
+        // GET: api/Student/5
+        public Students Get(int id)
+        {
+            Students student = new Students();
+
+            return student.studentsList().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/Student
+        public List<Students> Post(Students student)
+        {
+            List<Students> studentList = new List<Students>();
+            studentList.Add(student);
+
+            return studentList;
+        }
+
+        // PUT: api/Student/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Student/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/Students.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace webApp.Models
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public int Registry { get; set; }
+
+        public List<Students> studentsList()
+        {
+            var filePath = HostingEnvironment.MapPath(@"~/App_Data\Base.json");
+
+            var json = File.ReadAllText(filePath);
+
+            var studentsList = JsonConvert.DeserializeObject<List<Students>>(json);
+
+            return studentsList;
+        }
+    }
+}
